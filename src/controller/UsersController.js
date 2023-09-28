6423d7989b9c9b3b0e1c580e2f4c3f42df1ed040
damian-nestea <damian@hotmail.cz>
2023-09-28 18:58:44 -0300

    feat adding update user method on user class

--- src/controller/UsersController.js
+++ src/controller/UsersController.js
@@ -1,6 +1,6 @@
 const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 
 class UsersController {
   async create(request, response) {
@@ -28,6 +28,56 @@ class UsersController {
     );
 
     return response.status(201).json({ message: `User created succesfully` });
+  }
+
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", id);
+
+    if (!user) {
+      throw new AppError("User not found");
+    }
+
+    const userWithUpdatedEmail = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    console.log(id !== 3);
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Email already exists in our database");
+    }
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if (password && !old_password) {
+      throw new AppError(
+        "Provide both password and old password if you want to update it"
+      );
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if (!checkOldPassword) {
+        throw new AppError("Old password wrong. please try again");
+      }
+
+      user.password = await hash(password, 8);
+    }
+
+    await database.run(
+      "UPDATE users SET name = ?, email = ?,password = ?, updated_at = DATETIME('now', 'localtime') WHERE id = ?",
+      [user.name, user.email, user.password, id]
+    );
+
+    return response.json({
+      message: "User updated successfully",
+    });
   }
 }
 
