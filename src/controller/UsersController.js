02fcc839d279df0f74a9d40995e99f5ee50926a2
damian-nestea <damian@hotmail.cz>
2023-09-28 18:26:48 -0300

    feat adding create sqlite queries using db connection and hashed password checking if email already exists

--- src/controller/UsersController.js
+++ src/controller/UsersController.js
@@ -1,14 +1,33 @@
 const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
+const { hash } = require("bcryptjs");
 
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body;
-console.log(name)
+
     if (!name || !email || !password) {
       throw new AppError("Provide name, email and password");
     }
 
-    return response.json({ message: `User created succesfully` });
+    const database = await sqliteConnection();
+    const userExists = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    if (userExists) {
+      throw new AppError("Email already exists in database");
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES(?,?,?)",
+      [name, email, hashedPassword]
+    );
+
+    return response.status(201).json({ message: `User created succesfully` });
   }
 }
 
